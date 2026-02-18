ab336a1883962a38d75c465552a99528b95bd2b5
madhuvansh <madhuvanshsivakumar@gmail.com>
2026-02-18 16:42:59 +0530

    added update and delete

--- express.js
+++ express.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// PostgreSQL connection
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
@@ -16,10 +15,12 @@ const pool = new Pool({
   port: 5432,
 });
 
-// Route to get users
+// GET all active users
 app.get("/users", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM users");
+    const result = await pool.query(
+      "SELECT * FROM users WHERE isactive = true ORDER BY name"
+    );
     res.json(result.rows);
   } catch (err) {
     console.error(err);
@@ -27,23 +28,56 @@ app.get("/users", async (req, res) => {
   }
 });
 
+// INSERT user
 app.post("/users", async (req, res) => {
   try {
     const { name, email, age, gender, company } = req.body;
 
     const result = await pool.query(
-      "INSERT INTO users (name, email, age, gender, company) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      "INSERT INTO users (name, email, age, gender, company) VALUES ($1,$2,$3,$4,$5) RETURNING *",
       [name, email, age, gender, company]
     );
 
     res.status(201).json(result.rows[0]);
-
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Insert failed" });
   }
 });
 
+// SOFT DELETE using UUID
+app.delete("/users/:uuid", async (req, res) => {
+  try {
+    const { uuid } = req.params;
+
+    await pool.query(
+      "UPDATE users SET isactive = false WHERE uuid = $1",
+      [uuid]
+    );
+
+    res.json({ message: "User deleted successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Delete failed" });
+  }
+});
+// UPDATE USER
+app.put("/users/:uuid", async (req, res) => {
+  try {
+    const { uuid } = req.params;
+    const { name, email, age, gender, company } = req.body;
+
+    const result = await pool.query(
+      "UPDATE users SET name=$1, email=$2, age=$3, gender=$4, company=$5 WHERE uuid=$6 RETURNING *",
+      [name, email, age, gender, company, uuid]
+    );
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Update failed" });
+  }
+});
 
 app.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
